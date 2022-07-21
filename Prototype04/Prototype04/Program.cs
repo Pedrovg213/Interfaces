98fa963bbb2e5cf69ec8ab22d4c5c11d9fad25d5
Pedrovg213 <92651971+Pedrovg213@users.noreply.github.com>
2022-07-21 16:38:10 -0300

    Prototype04: IComparable
    - First: Reorganizing a name list in alphabetic order.

---
+++ Prototype04/Prototype04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Prototype04 {
+   internal class Program {
+      static void Main( string[ ] args ) {
+
+         string filePath = @"C:\Users\pedro\Desktop\C# Estudo\Seção 14 - Interfaces\Interfaces\Prototype04\in.txt";
+
+         try {
+            using ( StreamReader streamReader = File.OpenText( filePath ) ) {
+
+               List<string> list = new List<string>();
+
+               while ( !streamReader.EndOfStream )
+                  list.Add( streamReader.ReadLine() );
+
+               list.Sort();
+               foreach ( string str in list )
+                  Console.WriteLine( str );
+            }
+         } catch ( IOException ioe ) {
+
+            Console.WriteLine( "An error occurred." );
+            Console.WriteLine( ioe.Message );
+
+         }
+      }
+   }
+}
